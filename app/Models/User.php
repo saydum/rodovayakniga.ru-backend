c47ac66f8b4f67a6ef4f7a47ded89b26ed252043
Saydum <saydum.it5@gmail.com>
2023-11-20 20:11:49 +0300

    delete: all files

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -43,9 +42,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
-    public function rod(): HasMany
-    {
-        return$this->hasMany(Rod::class, 'user_id', 'id');
-    }
 }
