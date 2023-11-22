fae172e8c868cc92eb6d05cf5b1739fdc85b0db4
Saydum <saydum.it5@gmail.com>
2023-11-22 09:50:06 +0300

    refactor: removed are not used routes

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,7 @@
 <?php
 
-use App\Http\Controllers\HumanController;
-use App\Http\Controllers\RodController;
-use App\Http\Controllers\TreeController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::resource('rod', RodController::class);
-Route::resource('humans', HumanController::class);
-
-Route::get('/tree/{human}', [TreeController::class, 'index'])->name('tree.index');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome.index');
