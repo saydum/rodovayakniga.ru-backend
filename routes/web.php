c035488cb49f83f322bffe5e29b502edbf01759d
Saydum <saydum.it5@gmail.com>
2023-11-23 21:29:40 +0300

    feat: Add HumanController and TreeController to the list of imported controllers and add a new route for '/tree/{human}'.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\HumanController;
 use App\Http\Controllers\RodController;
+use App\Http\Controllers\TreeController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +10,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome.index');
 
 Route::resource('rod', RodController::class);
+Route::resource('humans', HumanController::class);
+
+Route::get('/tree/{human}', [TreeController::class, 'index'])->name('tree.index');
