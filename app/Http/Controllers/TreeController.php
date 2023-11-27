ac6916c52c6b015430d5248c716d3f0d6d5af51c
Saydum <saydum.it5@gmail.com>
2023-11-27 15:22:46 +0300

    refactor: TreeController.php to include additional information about family members.

--- app/Http/Controllers/TreeController.php
+++ app/Http/Controllers/TreeController.php
@@ -18,12 +18,30 @@ class TreeController extends Controller
 
     public function index(int $id)
     {
-        $human = $this->humanService->getHumanWithParentsById($id);
         $humans = $this->humanService->getAll();
 
+        $im = $this->humanService->getHumanWithParentsById($id);
+
+        $father = $im->father;
+        $mather = $im->mather;
+
+        $fatherGrandfather = $humans->find($im->father->id)->father;
+        $fatherGrandmother = $humans->find($im->father->id)->mather;
+
+        $matherGrandfather = $humans->find($im->mather->id)->father;
+        $matherGrandmother = $humans->find($im->mather->id)->mather;
+
+
+
+
         return view('tree.index', [
-            'human' => $human,
-            'humans' => $humans,
+            'im' => $im,
+            'father' => $father,
+            'mather' => $mather,
+            'fatherGrandfather' => $fatherGrandfather,
+            'fatherGrandmother' => $fatherGrandmother,
+            'matherGrandfather' => $matherGrandfather,
+            'matherGrandmother' => $matherGrandmother
         ]);
     }
 }
