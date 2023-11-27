e27aaa43cd4b38795c881ddf0333b8dfd8a8627f
Saydum <saydum.it5@gmail.com>
2023-11-27 15:21:46 +0300

    refactor: ui family tree

--- resources/views/tree/index.blade.php
+++ resources/views/tree/index.blade.php
@@ -5,78 +5,80 @@
 
     <div class="container">
         <div class="row">
-            <div class="tree">
-                <ul>
-                    <li>
-                        <a href="#">
-                            <img src="{{ asset($human->image) }}">
-                            <span>{{ $human->name }}</span>
-                        </a>
-
+            <div class="col">
+                <div class="tree">
+                    @isset($im)
                         <ul>
                             <li>
+                                {{-- 1 --}}
                                 <a href="#">
-                                    <img src="{{ asset($human->father->image) }}">
-                                    <span>{{ $human->father->name }}</span>
+                                    <img src="{{ asset($im->image) }}">
+                                    <span> {{ $im->name. " " .  $im->o_name . " " . $im->f_name}}</span>
                                 </a>
+                                {{-- E1--}}
                                 <ul>
-                                    <li>
-                                        <a href="#">
-                                            <img src="{{ asset($humans->find($human->father->id)->father->image) }}">
-                                            <span>
-                                                {{ $humans->find($human->father->id)->father->name }}
-                                            </span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            @isset($humans->find($human->father->id)->mather->image)
-                                                <img src="{{ asset($humans->find($human->father->id)->mather->image) }}">
-                                            @endisset
-                                            <span>
-                                                {{ $humans->find($human->father->id)->mather->name }}
-                                            </span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#">
-                                    <img src="{{ asset($human->mather->image) }}">
-                                    <span>{{ $human->mather->name }}</span>
-                                </a>
-                                <ul>
-                                    <li>
-                                        <a href="#">
-                                            <img src="{{ asset($humans->find($human->mather->id)->father->image) }}">
-                                            <span>{{ $humans->find($human->mather->id)->father->name }}</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <img src="{{ asset($humans->find($human->mather->id)->mather->image) }}">
-                                            <span> {{ $humans->find($human->mather->id)->mather->name }}</span>
-                                        </a>
-{{--                                        <ul>--}}
-{{--                                            <li>--}}
-{{--                                                <a href="#">--}}
-{{--                                                    <img src="images/7.jpg">--}}
-{{--                                                    <span>Hello</span>--}}
-{{--                                                </a>--}}
-{{--                                            </li>--}}
-{{--                                            <li>--}}
-{{--                                                <a href="#">--}}
-{{--                                                    <img src="images/7.jpg">--}}
-{{--                                                    <span>Hey</span>--}}
-{{--                                                </a>--}}
-{{--                                            </li>--}}
-{{--                                        </ul>--}}
-                                    </li>
+                                    @isset($father)
+                                        <li>
+                                            <a href="#">
+                                                <img src="{{ asset($father->image) }}">
+                                                <span>{{ $father->name . " " . $father->o_name . " " . $father->f_name }}</span>
+                                            </a>
+                                            <ul>
+                                                @isset($fatherGrandfather)
+                                                    <li>
+                                                        <a href="#">
+                                                            <img
+                                                                src="{{ asset($fatherGrandfather->image) }}"/>
+                                                            <span>{{ $fatherGrandfather->name }}</span>
+                                                        </a>
+                                                    </li>
+                                                @endisset
+                                                @isset($fatherGrandmother)
+                                                    <li>
+                                                        <a href="#">
+                                                            <img
+                                                                src="{{ asset($fatherGrandmother->image) }}">
+                                                            <span>{{ $fatherGrandmother->name }}</span>
+                                                        </a>
+                                                    </li>
+                                                @endisset
+                                            </ul>
+                                        </li>
+                                    @endisset
+
+                                    @isset($mather)
+                                        <li>
+                                            <a href="#">
+                                                <img src="{{ asset($mather->image) }}">
+                                                <span>{{ $mather->name . " " . $mather->o_name . " " . $mather->f_name }}</span>
+                                            </a>
+                                            <ul>
+                                                @isset($matherGrandfather)
+                                                    <li>
+                                                        <a href="#">
+                                                            <img
+                                                                src="{{ asset($matherGrandfather->image) }}">
+                                                            <span>{{ $matherGrandfather->name }}</span>
+                                                        </a>
+                                                    </li>
+                                                @endisset
+                                                @isset($matherGrandmother)
+                                                    <li>
+                                                        <a href="#">
+                                                            <img
+                                                                src="{{ asset($matherGrandmother->image) }}">
+                                                            <span> {{ $matherGrandmother->name }}</span>
+                                                        </a>
+                                                    </li>
+                                                @endisset
+                                            </ul>
+                                        </li>
+                                    @endisset
                                 </ul>
                             </li>
                         </ul>
-                    </li>
-                </ul>
+                    @endisset
+                </div>
             </div>
         </div>
     </div>
